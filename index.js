41d10dc4575e1c419ef992ab6d95e156d5bf4f1b
Pranjal Maurya <pranjalmaurya01@gmail.com>
2023-08-21 02:04:45 +0530

    testing express

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const User = require('./user.model');
+
 const app = express();
-const port = 5000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://arjunkaushik845:7gT6A8fZlHvTC8GK@cluster0.aov1eao.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-app.use(bodyParser.json());
-// Registration endpoint
-app.post('/register', async (req, res) => {
-  try {
-    const { name, email, engProLevel, mobNum, age } = req.body;
+app.get('/', (req, res) => res.send('Home Page Route'));
 
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-    const newUser = new User({ name, email, engProLevel, mobNum, age });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: error });
-  }
-});
+app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.get('/', async (req, res) => {
-  res.status(200).json({ message: 'Hello World' });
-})
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
